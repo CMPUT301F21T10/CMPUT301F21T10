c8502cf2717a729f48ca252bebc199d071888502
= <=>
2021-09-29 19:05:34 -0600

    Updated color

--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301f21t10;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    String color = "GREEN";
+    String color = "Purple";
     String properName = "Hank";
 }
