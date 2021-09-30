80f06e4f24b06fbb91059d3eb575fdf306167d59
davidhanwang <46507048+davidhanwang@users.noreply.github.com>
2021-09-29 19:12:14 -0600

    Update Shape.java

--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -4,6 +4,6 @@ public abstract class Shape {
     protected Integer x;
     protected Integer y;
 
-    String color = "Orange";
+    String color = "Violet(wang)";
     String properName = "Hank";
 }
