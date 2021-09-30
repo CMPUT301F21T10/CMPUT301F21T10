eb2b4d2dfd9e43ff7adf7dccff0e4b9a72a0fd0f
Aqil <91294196+v6k@users.noreply.github.com>
2021-09-29 19:52:49 -0600

    Added Shape Color

--- app/src/main/java/com/example/cmput301f21t10/Rectangle.java
+++ app/src/main/java/com/example/cmput301f21t10/Rectangle.java
@@ -3,7 +3,9 @@ package com.example.cmput301f21t10;
 public class Rectangle extends Shape { 
     private int length;
     private int height;
-    public Rectangle(Integer X, Integer Y);
-        this.length = x;
-        this.height = y;
+    public Rectangle(Integer X, Integer Y){
+        this.length = X;
+        this.height = Y;
+    }
+
 }
