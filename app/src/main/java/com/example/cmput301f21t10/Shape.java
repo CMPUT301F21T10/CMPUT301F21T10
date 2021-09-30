b03e2bf5aa75f6d3a84507a3029d5edf3b659565
yevheniy73 <yevhenkaznovskyi@gmail.com>
2021-09-29 18:55:26 -0600

    Blue Yevhen

--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -3,4 +3,5 @@ package com.example.cmput301f21t10;
 public abstract class Shape {
     public Integer x;
     public Integer y;
+    public String color = "blue";
 }
--- app/src/main/java/com/example/cmput301f21t10/Square.java
+++ app/src/main/java/com/example/cmput301f21t10/Square.java
@@ -4,6 +4,7 @@ public class Square {
     private final int x;
     private final int y;
 
+
     public Square(int x, int y) {
         this.x = x;
         this.y = y;
