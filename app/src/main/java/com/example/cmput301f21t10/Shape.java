6ba7e95ec9360e0e993e1a3e0e9719b22c879742
Raj Shreyas <penukond@ualberta.ca>
2021-09-29 19:05:06 -0600

    Changed

--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -3,6 +3,6 @@ package com.example.cmput301f21t10;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    String color = "GREEN";
-    String properName = "Hank";
+    String color = "NO";
+    String properName = "Raj";
 }
