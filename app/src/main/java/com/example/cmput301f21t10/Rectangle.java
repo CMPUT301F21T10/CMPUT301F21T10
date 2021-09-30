4d733ec0efaf8380d56bd72353729e780eb28284
Aqil <91294196+v6k@users.noreply.github.com>
2021-09-29 18:53:23 -0600

    Added Rectangle

---
+++ app/src/main/java/com/example/cmput301f21t10/Rectangle.java
@@ -0,0 +1,9 @@
+package com.example.cmput301f21t10;
+
+public class Rectangle extends Shape { 
+    private int length;
+    private int height;
+    public Rectangle(Integer X, Integer Y);
+        this.length = x;
+        this.height = y;
+}
