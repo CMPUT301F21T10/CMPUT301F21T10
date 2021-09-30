efe632663737a155b3e4c416a14fb88d68519c68
Raj Shreyas <penukond@ualberta.ca>
2021-09-29 18:37:51 -0600

    Circle

---
+++ app/src/main/java/com/example/cmput301f21t10/Circle.java
@@ -0,0 +1,10 @@
+package com.example.cmput301f21t10;
+
+public class Circle extends Shape{
+    private int radius;
+    public Circle(int radius, int x, int y){
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+}
--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -1,6 +1,6 @@
 package com.example.cmput301f21t10;
 
 public abstract class Shape {
-    private Integer x;
-    private Integer y;
+    protected Integer x;
+    protected Integer y;
 }
