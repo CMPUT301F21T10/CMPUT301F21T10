0c4fb7dc935cafbb7795084a9853989ccb295862
Raj Shreyas <penukond@ualberta.ca>
2021-09-29 18:55:03 -0600

    fixed woops

--- app/src/main/java/com/example/cmput301f21t10/Shape.java
+++ app/src/main/java/com/example/cmput301f21t10/Shape.java
@@ -3,5 +3,5 @@ package com.example.cmput301f21t10;
 public abstract class Shape {
     protected Integer x;
     protected Integer y;
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
+    String color = "GREEN";
 }
